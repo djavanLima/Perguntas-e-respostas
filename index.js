8d5edf943d131a5dcaa0d11d057c83e8e8608205
Djavan Lima <djavanlyonel@gmail.com>
2020-10-23 20:44:31 -0400

    realizada a conexão com o banco

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const app = express();
+const bodyparser=require("body-parser");
+const connection = require("./database/database");
+//Database
+    connection
+        .authenticate()
+        .then( ()=>{
+            console.log("Conexão feita com banco de dados!")
+        }).catch(msgErro=>{
+                console.log(msgErro);
+        });
 
 //para que se use o ejs como view engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+//bodypoarser
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+
+//rotas
 
 app.get('/',(req, res)=>{
 
@@ -15,7 +31,13 @@ app.get('/perguntar',(req,res)=>{
 });
 
 
-app.get('/salvarpergunta');
+app.post('/salvarpergunta',(req,res)=>{
+
+    let titulo=req.body.titulo;
+    let descricao= req.body.descricao;
+
+    res.send("formulário recebido!" + ` titulo: ${titulo} descricao:${descricao}`);
+});
 
 
 

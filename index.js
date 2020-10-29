931df9bdc122267bf1f6c1c0d6b2af7e4b62619f
Djavan Lima <djavanlyonel@gmail.com>
2020-10-28 23:28:01 -0400

    inserindo e listando respostas

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const bodyparser=require("body-parser");
 const connection = require("./database/database");
-const perguntaRepository = require("./model/Pergunta")
+const perguntaRepository = require("./model/Pergunta");
+const respostaRepository = require('./model/Resposta');
+
 //Database
     connection
         .authenticate()
@@ -24,7 +26,9 @@ app.use(bodyparser.json());
 //rotas
 
 app.get('/',(req, res)=>{
-   perguntaRepository.findAll({raw:true}).then(perguntas=>{
+   perguntaRepository.findAll({raw:true,order:[
+       ['id','DESC']// ORDENAÇÃO POR ID E DECRESCENTE
+   ] }).then(perguntas=>{
 
         res.render('index',{
             perguntas:perguntas
@@ -44,12 +48,57 @@ app.post('/salvarpergunta',(req,res)=>{
     let titulo=req.body.titulo;
     let descricao= req.body.descricao;
 
-    perguntaRepository.create({
+    respostaRepository.create({
         titulo:titulo,
         descricao:descricao
     }).then(()=>{   //redireciona o usuario para a tela inicial, caso seja true
         res.redirect("/");
     });
+});
+
+
+//redireciona para a pagina sobre a perguna em questão
+
+app.get('/pergunta/:id',(req,res)=>{
+    var id = req.params.id;
+    perguntaRepository.findOne({
+        where:{id:id}// condição para selecionar por id, entretanto, posso selecionar por qualquer coisa
+    }).then(pergunta=>{
+        if(pergunta!= undefined)// pergunta encontrada
+        {
+
+            respostaRepository.findAll({
+                where:{perguntaId: pergunta.id}
+            }).then(respostas =>{
+
+                res.render('pergunta',{
+                    pergunta:pergunta,
+                    respostas:respostas
+                });
+
+            } );
+
+
+            
+        }else
+        {//não encontrado
+            res.redirect('/'); //redireciona
+        }
+    });
+
+});
+
+
+
+app.post("/responder", (req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+    respostaRepository.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect("/pergunta/"+perguntaId);
+    })
 });
 
 
